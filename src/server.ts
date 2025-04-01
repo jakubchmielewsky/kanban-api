55bc6bfce9e29433fd27079d077b05b8d1496589
Jakub Chmielwski <kuba91821@gmail.com>
2025-04-01 13:02:35 +0200

    add uncaughtException and unhandledRejection handling

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,11 @@ import { connectDB } from "./config/db";
 
 dotenv.config({ path: ".env" });
 
+process.on("uncaughtException", (err) => {
+  console.error("💥 Uncaught exception:", err);
+  process.exit(1);
+});
+
 import app from "./app";
 
 connectDB();
@@ -11,4 +16,11 @@ const PORT = process.env.PORT || 3000;
 
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}...`);
+});
+
+process.on("unhandledRejection", (reason) => {
+  console.error("💥 Unhandled rejection:", reason);
+  server.close(() => {
+    process.exit(1);
+  });
 });
