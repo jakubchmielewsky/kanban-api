cc573324901f066d1ac4f042954a5f4e62d4540a
Jakub Chmielwski <kuba91821@gmail.com>
2025-04-30 19:08:11 +0200

    feat: add event emitting to create and delete task controllers

--- src/controllers/tasksController.ts
+++ src/controllers/tasksController.ts
@@ -1,14 +1,11 @@
-import {
-  createOne,
-  getAll,
-  deleteOne,
-  updateOne,
-  getOne,
-} from "./handlerFactory";
+import { getAll, deleteOne, updateOne, getOne } from "./handlerFactory";
 import Task from "../models/TaskModel";
 import catchAsync from "../utils/catchAsync";
 import { Request, Response, NextFunction } from "express";
 import { io } from "../server";
+import AppError from "../utils/AppError";
+import mongoose from "mongoose";
+import { cascadeDelete } from "../utils/cascadeDelete";
 
 export const getAllTasks = getAll(Task);
 export const getTask = getOne(Task);
@@ -20,14 +17,10 @@ export const createTask = catchAsync(
 
     const socketId = req.headers["x-socket-id"];
 
-    console.log("Request comes from socket:", socketId);
-
     if (typeof socketId === "string") {
       io.to(task.boardId.toString())
         .except(socketId)
         .emit("task_created", task);
-    } else {
-      io.to(task.boardId.toString()).emit("task_created", task);
     }
 
     res.status(201).json({
@@ -36,5 +29,71 @@ export const createTask = catchAsync(
     });
   }
 );
-export const updateTask = updateOne(Task);
-export const deleteTask = deleteOne(Task);
+export const updateTask = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!task) {
+      return next(new AppError("No document found with that ID", 404));
+    }
+
+    const socketId = req.headers["x-socket-id"];
+
+    if (typeof socketId === "string") {
+      io.to(task.boardId.toString())
+        .except(socketId)
+        .emit("task_updated", task);
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        data: task,
+      },
+    });
+  }
+);
+export const deleteTask = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const session = await mongoose.startSession();
+
+    try {
+      session.startTransaction();
+
+      const task = await Task.findById(req.params.id).session(session);
+
+      if (!task) {
+        await session.abortTransaction();
+        return next(new AppError("No document found with that ID", 404));
+      }
+
+      await cascadeDelete(session, Task, req.params.id);
+
+      await Task.findByIdAndDelete(req.params.id).session(session);
+
+      await session.commitTransaction();
+
+      const socketId = req.headers["x-socket-id"];
+
+      if (typeof socketId === "string") {
+        io.to(task.boardId.toString())
+          .except(socketId)
+          .emit("task_deleted", task._id);
+      }
+
+      res.status(204).json({
+        status: "success",
+        data: null,
+      });
+    } catch (error) {
+      await session.abortTransaction();
+
+      return next(error);
+    } finally {
+      await session.endSession();
+    }
+  }
+);
